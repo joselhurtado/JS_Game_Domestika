0e3566d25e6b8a00b81a94d79af801d0e3998872
Jose Hurtado <hello@hurtadojose.com>
2022-07-06 04:11:19 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var cardsGroup = ["🦄", "🍦", "🌈", "👽", "👾", "🤖", "👹", "👺"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+var totalCards = cardsGroup.concat(cardsGroup);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function mixAllCards() {
+  var result;
+  result = totalCards.sort(function() {
+    return 0.5 - Math.random();
+  });
+  return result;
+}
+
+function cardsDealer() {
+  var table = document.querySelector("#table");
+  var mixCards = mixAllCards();
+  table.innerHTML = "";
+
+  mixCards.forEach(function(emoji) {
+    var singleCard = document.createElement("div");
+
+    singleCard.innerHTML =
+      "<div class='singleCard'>" +
+      "<div class='card_Content'>" +
+      emoji +
+      "</div>" +
+      "</div>";
+
+    table.appendChild(singleCard);
+  });
+}
+
+function flipCard() {
+  this.classList.add("flip");
+}
+
+cardsDealer();
+
+document.querySelectorAll(".singleCard").forEach(function(emoji) {
+  emoji.addEventListener("click", flipCard);
+});
